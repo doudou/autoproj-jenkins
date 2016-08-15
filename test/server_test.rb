ae6d1d4016b1102fedf5ecc1d4c53b669d48b4ad
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-08-15 09:49:47 -0300

    test: fix access to the server API without the TEST_JOB_PREFIX prepended to job names

--- test/server_test.rb
+++ test/server_test.rb
@@ -7,11 +7,14 @@ module Autoproj::Jenkins
                 jenkins_connect
             end
             after do
-                jenkins_delete_job name
+                begin
+                    jenkins_delete_job name
+                rescue JenkinsApi::Exceptions::NotFound
+                end
             end
 
             it "creates a job with the given name and template" do
-                server.create_job(name, 'empty_job.xml',
+                server.create_job(TestHelper::TEST_JOB_PREFIX + name, 'empty_job.xml',
                                   description: 'job creation test',
                                   template_path: template_path)
                 assert jenkins_has_job?(name)
