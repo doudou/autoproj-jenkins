e46dcdd5617e6fd013cd5a360900e4ef3ba4cdf6
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-08-18 12:30:29 -0300

    test: use the 'dev' mode
    This ensures that we can still run the tests if some
    autoproj/autoproj-jenkins APIs change. In this instance, tests were
    broken because of the --skip-stage2 flag.

--- test/updater_test.rb
+++ test/updater_test.rb
@@ -22,6 +22,12 @@ module Autoproj::Jenkins
             base_logging.autobuild.depends_on 'base/cmake'
         end
 
+        def update_options
+            Hash[quiet_period: 0,
+                 gemfile: 'buildconf-vagrant-Gemfile',
+                 autoproj_install_path: '/opt/autoproj/bin/autoproj_install']
+        end
+
         after do
             FileUtils.rm_rf workspace_dir
         end
@@ -32,11 +38,11 @@ module Autoproj::Jenkins
                 @updater = Updater.new(ws, jenkins_connect, job_prefix: TestHelper::TEST_JOB_PREFIX)
             end
             it "creates the buildconf job if it does not exist" do
-                updater.create_or_update_buildconf_job(gemfile: 'buildconf-vagrant-Gemfile')
+                updater.create_or_update_buildconf_job(**update_options)
                 assert jenkins_has_job?('buildconf')
             end
             it "restricts itself to the packages given on the command line and its dependencies" do
-                updater.create_or_update_buildconf_job(base_logging, quiet_period: 0, gemfile: 'buildconf-vagrant-Gemfile')
+                updater.create_or_update_buildconf_job(base_logging, **update_options)
                 jenkins_run_job 'buildconf'
                 assert jenkins_has_job?('base-cmake')
                 assert jenkins_has_job?('base-logging')
@@ -46,14 +52,14 @@ module Autoproj::Jenkins
             it "raises if the autoproj buildconf has no vcs" do
                 ws.manifest.vcs = Autoproj::VCSDefinition.none
                 assert_raises(ArgumentError) do
-                    updater.create_or_update_buildconf_job(base_logging, quiet_period: 0)
+                    updater.create_or_update_buildconf_job(base_logging, **update_options)
                 end
                 refute jenkins_has_job?('base-logging')
             end
             it "raises if the autoproj buildconf has a local vcs" do
                 ws.manifest.vcs = Autoproj::VCSDefinition.from_raw('type' => 'local', 'url' => '/test')
                 assert_raises(ArgumentError) do
-                    updater.create_or_update_buildconf_job(base_logging, quiet_period: 0)
+                    updater.create_or_update_buildconf_job(base_logging, **update_options)
                 end
                 refute jenkins_has_job?('base-logging')
             end
@@ -66,7 +72,7 @@ module Autoproj::Jenkins
             end
 
             it "handles git importers" do
-                updater.update(base_cmake, quiet_period: 0)
+                updater.update(base_cmake, **update_options)
                 jenkins_run_job 'base-cmake'
             end
 
@@ -76,18 +82,18 @@ module Autoproj::Jenkins
                 package = ws.register_package(package, nil)
                 package.vcs = Autoproj::VCSDefinition.from_raw(
                     type: :archive, url: 'https://github.com/SINTEF-Geometry/SISL/archive/SISL-4.6.0.tar.gz')
-                updater.update(package, quiet_period: 0)
+                updater.update(package, **update_options)
                 jenkins_run_job 'external-sisl'
             end
 
             it "handles having source directories not matching the package name" do
                 base_cmake.autobuild.srcdir = File.join(workspace_dir, 'test')
-                updater.update(base_cmake, quiet_period: 0)
+                updater.update(base_cmake, **update_options)
                 jenkins_run_job 'base-cmake'
             end
 
             it "creates a job that runs successfully" do
-                updater.update(base_cmake, quiet_period: 0)
+                updater.update(base_cmake, **update_options)
                 jenkins_run_job 'base-cmake'
             end
 
@@ -102,14 +108,14 @@ module Autoproj::Jenkins
             end
 
             it "handles dependencies between packages" do
-                updater.update(base_cmake, base_logging, quiet_period: 0)
+                updater.update(base_cmake, base_logging, **update_options)
                 jenkins_start_job 'base-cmake'
                 jenkins_join_job 'base-cmake'
                 jenkins_join_job 'base-logging'
             end
 
             it "waits for upstream jobs to finish" do
-                updater.update(base_cmake, base_logging, quiet_period: 0)
+                updater.update(base_cmake, base_logging, **update_options)
                 jenkins_start_job 'base-cmake'
                 # The 'base-logging' job would fail without synchronization
                 # because it relies on the existence of artifacts that are not
